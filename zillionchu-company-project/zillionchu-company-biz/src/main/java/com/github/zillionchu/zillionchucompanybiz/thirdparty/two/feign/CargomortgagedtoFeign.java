02d44079bf094e4141e2868d63455021d41eb6bb
duzhongyuan <duzhongyuan@echinacoop.com>
2019-12-25 10:45:07 +0800

    生成feign

---
+++ zillionchu-company-project/zillionchu-company-biz/src/main/java/com/github/zillionchu/zillionchucompanybiz/thirdparty/two/feign/CargomortgagedtoFeign.java
@@ -0,0 +1,20 @@
+package com.github.zillionchu.zillionchucompanybiz.thirdparty.two.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+/**
+ * @Auther: ZiLlionChu
+ * @Date: 2019/12/19 15:47
+ * @Description:
+ */
+@FeignClient("zilionchu-server-http02")
+public interface CargomortgagedtoFeign {
+
+    @PostMapping("indexPost")
+    public Object indexPost(@RequestBody Map param);
+
+}
